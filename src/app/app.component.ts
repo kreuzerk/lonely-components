074c174b4e70606dc36b61f26ff983db25fdc7d0
kreuzerk <kevin.kreuzer90@icloud.com>
2022-05-09 13:10:43 +0200

    feat: 🎸 (lazy loaded) lazy load route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { FooComponent } from './foo/foo.component';
+
+const routes = [
+  {
+    path: 'foo',
+    loadComponent: () =>
+      import('./foo/foo.component').then((c) => c.FooComponent),
+  },
+];
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule, RouterModule, FooComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
 })
 export class AppComponent {
   title = 'lonely-components';
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { RouterModule } from '@angular/router';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
